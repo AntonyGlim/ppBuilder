0ea0cb035465c62b8116ad5ff1912738f5e1c492
Antony.Glim <Antony.Glim@gmail.com>
2019-06-25 23:19:35 +0300

    adding log4j in process

--- src/main/java/pp/Main.java
+++ src/main/java/pp/Main.java
@@ -6,11 +6,18 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.LoggerFactory;
+
+
 public class Main {
 
     private static List<String> fileNames = new ArrayList<>();
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) {
+        logger.debug("Hello this is a debug message");
+        logger.info("Hello this is an info message");
         try {
             //read list of files from the file txt
             BufferedReader bufferedReader = new BufferedReader(new FileReader("list.txt"));
